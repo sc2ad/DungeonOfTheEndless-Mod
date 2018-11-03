0792c3bc2bbc3c39683d603724afc11e796900f8
sc2ad <adamznow@gmail.com>
2018-11-02 21:05:58 -0700

    Initial Commit

---
+++ ModManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotE_Mod
+{
+    public class ModManager
+    {
+        public static bool Enabled { get; set; } = true;
+        public static bool UseRandomRooms { get; set; } = false;
+
+        public static int GetDoors(int low, int high)
+        {
+            if (Enabled)
+            {
+                // Do some calculation manually
+
+            }
+            return RandomGenerator.RangeInt(low, high);
+        }
+    }
+}
