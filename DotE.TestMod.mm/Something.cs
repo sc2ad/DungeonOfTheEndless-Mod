6ef5d18d9ffa21a369127607c7f1d79b87caf991
Sc2ad <adamznow@gmail.com>
2018-11-07 10:13:49 -0800

    Created new projects with use for partiality and monomod

---
+++ DotE.TestMod.mm/Something.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonoMod;
+
+#pragma warning disable CS0626
+
+
+namespace DotE.TestMod.mm
+{
+    class Something
+    {
+
+    }
+}
