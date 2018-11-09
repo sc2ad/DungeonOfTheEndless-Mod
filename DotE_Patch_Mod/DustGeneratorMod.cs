237fab67c825f423a14530453624dc74279e49c8
Unknown <adamznow@gmail.com>
2018-11-09 13:48:24 -0800

    Added DustGenerator and ScadMod for base framework
    DustGenerator now works, also in multiplayer! Also has a handy config file!

---
+++ DotE_Patch_Mod/DustGeneratorMod.cs
@@ -0,0 +1,65 @@
+﻿using MonoMod.Utils;
+using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotE_Patch_Mod
+{
+    class DustGeneratorMod : PartialityMod
+    {
+        ScadMod mod = new ScadMod();
+        public override void Init()
+        {
+            mod.path = @"C:\Program Files (x86)\Steam\steamapps\common\Dungeon of the Endless\DustGenerator_log.txt";
+            mod.config = @"DustGenerator_config.txt";
+            mod.Initalize();
+
+            // Setup default values for config
+            mod.Values.Add("DustPerDoor", "10.0");
+            mod.Values.Add("DustFromProducing", "False");
+            mod.Values.Add("DustFromRoom", "False");
+
+            mod.ReadConfig();
+
+            mod.Log("Initialized!");
+        }
+        public override void OnLoad()
+        {
+            mod.Load();
+            if (Convert.ToBoolean(mod.Values["Enabled"]))
+            {
+                On.Dungeon.GetDustProd += Dungeon_GetDustProd;
+                On.Room.Open += Room_Open;
+            }
+        }
+
+        private void Room_Open(On.Room.orig_Open orig, Room self, Door openingDoor, bool ignoreVisibility)
+        {
+            if (Convert.ToBoolean(mod.Values["DustFromRoom"]))
+            {
+                // You can reuse a DynData wrapper for multiple get / set operations on the same object
+                new DynData<Room>(self).Set<int>("DustLootAmount", (int)Convert.ToDouble(mod.Values["DustPerDoor"])); // Sets the dust value of this room to 10
+                mod.Log("Attempting to spawn: " + self.DustLootAmount + " dust in room!");
+                orig(self, openingDoor, ignoreVisibility);
+                return;
+            }
+            orig(self, openingDoor, ignoreVisibility);
+            mod.Log("Using default room collection: " + self.DustLootAmount);
+        }
+
+        private float Dungeon_GetDustProd(On.Dungeon.orig_GetDustProd orig, Dungeon self)
+        {
+            if (Convert.ToBoolean(mod.Values["DustFromProducing"]))
+            {
+                mod.Log("Attempting to Produce: " + Convert.ToDouble(mod.Values["DustPerDoor"]) + " dust!");
+                orig(self);
+                return (float)Convert.ToDouble(mod.Values["DustPerDoor"]);
+            }
+            mod.Log("Using default dust production..." + orig(self));
+            return orig(self);
+        }
+    }
+}
---
+++ DotE_Patch_Mod/ScadMod.cs
@@ -0,0 +1,71 @@
+﻿using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotE_Patch_Mod
+{
+    class ScadMod
+    {
+        public string path = @"placeholder_log.txt";
+
+        public string config = @"placeholder_config.txt";
+        public string default_config = "# Modify this file to change various settings of the DustGenerator Mod for DotE.\n# If these values are removed, the game will crash on initialization!\n";
+
+        public Dictionary<string, string> Values = new Dictionary<string, string>();
+
+        public void Log(string s)
+        {
+            System.IO.File.AppendAllText(path, s + "\n");
+        }
+        public void ClearLog()
+        {
+            System.IO.File.WriteAllText(path, "");
+        }
+        public void ReadConfig()
+        {
+            Log("Attempting to read from config file: " + config);
+            if (!System.IO.File.Exists(config))
+            {
+                Log("Need to create a new config file at: " + config);
+                string s = default_config;
+                foreach (string q in Values.Keys)
+                {
+                    s += q + ": " + Values[q] + "\n";
+                }
+                System.IO.File.WriteAllText(config, s);
+            }
+            string[] lines = System.IO.File.ReadAllLines(config);
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("#") || line.IndexOf(": ") == -1)
+                {
+                    continue;
+                }
+                string[] spl = line.Split(new string[] { ": " }, StringSplitOptions.None);
+                string value = spl[1].Trim();
+                Values[spl[0]] = value;
+                Log("Loaded: " + spl[0] + " = " + Values[spl[0]] + " from config");
+            }
+            Log("Values have been loaded successfully!");
+        }
+        public void Initalize()
+        {
+            ClearLog();
+            Log("===================================================================");
+            Log("DATETIME: " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToShortTimeString());
+            Log("===================================================================");
+
+            Values.Add("Enabled", "True");
+            // Add values here to the Values dictionary!
+        }
+        public void Load()
+        {
+            Log("Loaded!");
+            
+            // Load/Create hooks here!
+        }
+    }
+}
