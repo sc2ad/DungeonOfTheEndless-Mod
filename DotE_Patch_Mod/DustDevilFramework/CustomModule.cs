efa6eb8f2d5837de3544baedbea6993894164eb9
Adam ? <adamznow@gmail.com>
2019-02-11 02:11:43 -0800

    Added some stuff to CustomModule
    Needs tons of work, have hardly started.
    Among the massive list of todos:
    - Make sure animation clips are overwritten properly
    - Make sure Module is added to DB properly
    - Make sure a selectable option appears for the Module properly (as in you can build it)
    - Make sure icons appear properly (such as when researching)
    - Confirm that BPConfig and ModuleConfig properly make the object exist
    - Test in multiplayer

--- DotE_Patch_Mod/DustDevilFramework/CustomModule.cs
+++ DotE_Patch_Mod/DustDevilFramework/CustomModule.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DustDevilFramework
 {
@@ -20,15 +18,44 @@ namespace DustDevilFramework
         public abstract string GetDescription();
         // Returns the number of levels the module has
         public abstract int GetLevels();
+        // Returns the model of the module (should already exist)
+
+        // Module Config
+        // Returns the base cost (industry) of the module
+        public abstract int GetInitialCost();
+        // Returns the increase in cost for each existing such module on the current floor
+        public abstract int GetCostIncrement();
+        // Returns the number of seconds the module takes to build, when there are waves
+        public abstract int GetBuildDuration();
+        // Returns the attack type for attacking modules.
+        // Currently there are only certain attack types: Laser, Tesla, Bomb, ModuleGun, Sebastroll, ScienceLaser, TroubleMaker, Posion
+        // If the modules does not attack, return an empty string.
+        public abstract string GetAttackType();
+        // Returns whether the module attracts a merchant or not
+        public abstract bool GetAttractsMerchant();
+        // Returns whether the module powers a room or not
+        public abstract bool GetPowersRoom();
+        // Returns whether the module needs room power or not (default should be true)
+        public abstract bool GetNeedRoomPower();
+        // Returns whether the module rotates in order to attack
+        public abstract bool GetRotate();
+        // Returns whether the modules is removable or not
+        public abstract bool GetUnRemovable();
+
+        // Blueprint Config
+        // Returns the module category the module is
+        public abstract ModuleCategory GetModuleCategory();
+        // Returns an array with length = GetLevels() that holds the research costs for each level
+        public abstract int[] GetResearchCosts();
 
         public CustomModule(Type settingsType)
         {
             name = GetName();
-            settings = (CustomModuleSettings)settingsType.TypeInitializer.Invoke(new object[] { name });
+            settings = Activator.CreateInstance(settingsType, new object[] { name }) as CustomModuleSettings;
             this.settingsType = settingsType;
         }
 
-        public void Initialize()
+        public new void Initialize()
         {
             base.Initialize();
 
@@ -38,7 +65,6 @@ namespace DustDevilFramework
             if (settings.Enabled)
             {
                 Log("Attempting to create Localization changes!");
-                //CreateLocalizationChanges();
                 List<string> linesLst = new List<string>();
                 linesLst.Add("  <LocalizationPair Name=\"%" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "_Title\">" + GetTitle() + "</LocalizationPair>");
                 for (int i = 1; i <= GetLevels(); i++)
@@ -46,17 +72,33 @@ namespace DustDevilFramework
                     linesLst.Add("  <LocalizationPair Name=\"%" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "_LVL" + i + "_Title\">" + GetTitle() + " " + i + "</LocalizationPair>");
                 }
                 linesLst.Add("  <LocalizationPair Name=\"%" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "_Description\">" + GetDescription() + "</LocalizationPair>");
-                Util.ApplyLocalizationChange("%MinorModule_Minor0020_Description", 0, linesLst.ToArray());
+                Util.ApplyLocalizationChange("%MinorModule_Minor0020_Description", 0, linesLst);
+                // ModuleConfig
+                linesLst = new List<string>();
+                linesLst.Add("  <ModuleConfig Name=\"" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "\" AITargetType=\"" + GetModuleType() + "\" SimClass=\"" + GetModuleType() + "\" BuildBaseIndustryCost=\""
+                    + GetInitialCost() + "\" BuildIndustryCostIncrement=\"" + GetCostIncrement() + "\" BuildDuration=\"" + GetBuildDuration() + (GetAttackType().Length != 0 ? "\" AttackType=\"" + GetAttackType() + "\"" : "\"")
+                    + (GetAttractsMerchant() ? " AttractMerchant=\"1\"" : "") + (GetPowersRoom() ? " PowersRoom=\"true\"" : "") + (GetNeedRoomPower() ? "" : " NeedRoomPower=\"0\"") + (GetRotate() ? " Rotate=\"1\"" : "")
+                    + (GetUnRemovable() ? " Unremovable=\"1\"" : "") + "/>");
+                Util.ApplyFileChange(@"Public\Configuration\ModuleConfigs.xml", "Minor0017", 1, linesLst);
+                // BluePrintConfig
+                linesLst = new List<string>();
+                for (int i = 1; i <= GetLevels(); i++) {
+                    linesLst.Add("  <BluePrintConfig Name=\"" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "_LVL" + i + "\" ModuleCategory=\"" + GetModuleCategory() + "\" ModuleName=\""
+                        + GetModuleType() + "Module_" + GetModuleType() + GetID() + "\" ModuleLevel=\"" + i + "\" ResearchScienceCost=\"" + GetResearchCosts()[i - 1] + "\"/>");
+                }
+                Util.ApplyFileChange(@"Public\Configuration\BluePrintConfigs.xml", "Minor0017", 1, linesLst);
             } else
             {
                 Log("Attempting to remove Localization changes!");
                 Util.RemoveLocalizationChangeInclusive(GetModuleType() + "Module_", GetModuleType() + "Module_" + GetModuleType() + GetID() + "_Description");
+                Util.RemoveFileChangeInclusive(@"Public\Configuration\ModuleConfigs.xml", GetID(), GetID());
+                Util.RemoveFileChangeInclusive(@"Public\Configuration\BluePrintConfigs.xml", GetID(), GetID());
                 // Remove Localization Changes
             }
             Log("Initialized!");
         }
 
-        public void Load()
+        public new void Load()
         {
             base.Load();
             if (settings.Enabled)
@@ -64,7 +106,7 @@ namespace DustDevilFramework
 
             }
         }
-        public void UnLoad()
+        public new void UnLoad()
         {
             base.UnLoad();
 
