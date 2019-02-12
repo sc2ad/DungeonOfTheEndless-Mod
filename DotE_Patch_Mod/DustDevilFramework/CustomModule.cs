2ff8c09983a8e75b1e4f0b52df3b56aa27a4338b
Adam ? <adamznow@gmail.com>
2019-02-12 02:01:49 -0800

    Updated CustomModule it now works in game
    BUT! It has yet to properly impersonate another module, so you can research it if you spot the cost difference, but no new icon appears. You can see it in the build menu, as it costs the proper amount, but no icon shows for the module. Clicking the module and attempting to place it causes a crash, as there seems to be no prefab loaded for the module either.
    However, need to confirm more information on what is causing the CustomModule to crash.

--- DotE_Patch_Mod/DustDevilFramework/CustomModule.cs
+++ DotE_Patch_Mod/DustDevilFramework/CustomModule.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Amplitude.Unity.Framework;
+using MonoMod.Utils;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,52 +9,108 @@ namespace DustDevilFramework
     public abstract class CustomModule : ScadMod
     {
         public abstract string GetName();
-
-        // Returns the ModuleType this class represents
+        /// <summary>
+        /// Returns the ModuleType this class represents
+        /// </summary>
+        /// <returns></returns>
         public abstract ModuleType GetModuleType();
-        // Returns a 4-character digit that contains no overlap with existing IDs. Ex: 5555
+        /// <summary>
+        /// Returns a 4-character digit that contains no overlap with existing IDs. Ex: 5555
+        /// </summary>
+        /// <returns></returns>
         public abstract string GetID();
-        // Returns the Title of the Module
+        /// <summary>
+        /// Returns the Title of the Module
+        /// </summary>
+        /// <returns></returns>
         public abstract string GetTitle();
-        // Returns the description of the Module
+        /// <summary>
+        /// Returns the description of the Module
+        /// </summary>
+        /// <returns></returns>
         public abstract string GetDescription();
-        // Returns the number of levels the module has
+        /// <summary>
+        /// Returns the number of levels the module has
+        /// </summary>
+        /// <returns></returns>
         public abstract int GetLevels();
-        // Returns the model of the module (should already exist)
 
         // Module Config
-        // Returns the base cost (industry) of the module
-        public abstract int GetInitialCost();
-        // Returns the increase in cost for each existing such module on the current floor
-        public abstract int GetCostIncrement();
-        // Returns the number of seconds the module takes to build, when there are waves
-        public abstract int GetBuildDuration();
-        // Returns the attack type for attacking modules.
-        // Currently there are only certain attack types: Laser, Tesla, Bomb, ModuleGun, Sebastroll, ScienceLaser, TroubleMaker, Posion
-        // If the modules does not attack, return an empty string.
+        /// <summary>
+        /// Returns the base cost (industry) of the module
+        /// </summary>
+        /// <returns></returns>
+        public abstract float GetInitialCost();
+        /// <summary>
+        /// Returns the increase in cost for each existing such module on the current floor
+        /// </summary>
+        /// <returns></returns>
+        public abstract float GetCostIncrement();
+        /// <summary>
+        /// Returns the number of seconds the module takes to build, when there are waves
+        /// </summary>
+        /// <returns></returns>
+        public abstract float GetBuildDuration();
+        /// <summary>
+        /// Returns the attack type for attacking modules.
+        /// Currently there are only certain attack types: 
+        /// Laser, Tesla, Bomb, ModuleGun, Sebastroll, ScienceLaser, TroubleMaker, Posion
+        /// If the modules does not attack, return an empty string.
+        /// </summary>
+        /// <returns></returns>
         public abstract string GetAttackType();
-        // Returns whether the module attracts a merchant or not
+        /// <summary>
+        /// Returns whether the module attracts a merchant or not
+        /// </summary>
+        /// <returns></returns>
         public abstract bool GetAttractsMerchant();
-        // Returns whether the module powers a room or not
+        /// <summary>
+        /// Returns whether the module powers a room or not
+        /// </summary>
+        /// <returns></returns>
         public abstract bool GetPowersRoom();
-        // Returns whether the module needs room power or not (default should be true)
+        /// <summary>
+        /// Returns whether the module needs room power or not (default should be true)
+        /// </summary>
+        /// <returns></returns>
         public abstract bool GetNeedRoomPower();
-        // Returns whether the module rotates in order to attack
+        /// <summary>
+        /// Returns whether the module rotates in order to attack
+        /// </summary>
+        /// <returns></returns>
         public abstract bool GetRotate();
-        // Returns whether the modules is removable or not
-        public abstract bool GetUnRemovable();
+        /// <summary>
+        /// Returns whether the modules is removable or not
+        /// </summary>
+        /// <returns></returns>
+        public abstract bool GetUnremovable();
 
         // Blueprint Config
-        // Returns the module category the module is
+        /// <summary>
+        /// Returns the module category the module is
+        /// </summary>
+        /// <returns></returns>
         public abstract ModuleCategory GetModuleCategory();
-        // Returns an array with length = GetLevels() that holds the research costs for each level
-        public abstract int[] GetResearchCosts();
+        /// <summary>
+        /// Returns an array with Length = GetLevels() that holds the research costs for each level
+        /// </summary>
+        /// <returns></returns>
+        public abstract float[] GetResearchCosts();
+        /// <summary>
+        /// Returns the SimulationDescriptorWrapper for any effects that are desired
+        /// (default should be null)
+        /// </summary>
+        public abstract SimDescriptorWrapper GetSimulationDescriptor();
+        /// <summary>
+        /// Returns the Module to use for animations.
+        /// MUST BE IN THE FORMAT: MinorModule_Minor0017
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetAssumedModuleName();
 
-        public CustomModule(Type settingsType)
+        public CustomModule(string name, Type settingsType, Type partialityType) : base(name, settingsType, partialityType)
         {
-            name = GetName();
             settings = Activator.CreateInstance(settingsType, new object[] { name }) as CustomModuleSettings;
-            this.settingsType = settingsType;
         }
 
         public new void Initialize()
@@ -73,26 +131,26 @@ namespace DustDevilFramework
                 }
                 linesLst.Add("  <LocalizationPair Name=\"%" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "_Description\">" + GetDescription() + "</LocalizationPair>");
                 Util.ApplyLocalizationChange("%MinorModule_Minor0020_Description", 0, linesLst);
-                // ModuleConfig
-                linesLst = new List<string>();
-                linesLst.Add("  <ModuleConfig Name=\"" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "\" AITargetType=\"" + GetModuleType() + "\" SimClass=\"" + GetModuleType() + "\" BuildBaseIndustryCost=\""
-                    + GetInitialCost() + "\" BuildIndustryCostIncrement=\"" + GetCostIncrement() + "\" BuildDuration=\"" + GetBuildDuration() + (GetAttackType().Length != 0 ? "\" AttackType=\"" + GetAttackType() + "\"" : "\"")
-                    + (GetAttractsMerchant() ? " AttractMerchant=\"1\"" : "") + (GetPowersRoom() ? " PowersRoom=\"true\"" : "") + (GetNeedRoomPower() ? "" : " NeedRoomPower=\"0\"") + (GetRotate() ? " Rotate=\"1\"" : "")
-                    + (GetUnRemovable() ? " Unremovable=\"1\"" : "") + "/>");
-                Util.ApplyFileChange(@"Public\Configuration\ModuleConfigs.xml", "Minor0017", 1, linesLst);
-                // BluePrintConfig
-                linesLst = new List<string>();
-                for (int i = 1; i <= GetLevels(); i++) {
-                    linesLst.Add("  <BluePrintConfig Name=\"" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "_LVL" + i + "\" ModuleCategory=\"" + GetModuleCategory() + "\" ModuleName=\""
-                        + GetModuleType() + "Module_" + GetModuleType() + GetID() + "\" ModuleLevel=\"" + i + "\" ResearchScienceCost=\"" + GetResearchCosts()[i - 1] + "\"/>");
-                }
-                Util.ApplyFileChange(@"Public\Configuration\BluePrintConfigs.xml", "Minor0017", 1, linesLst);
+                //// ModuleConfig
+                //linesLst = new List<string>();
+                //linesLst.Add("  <ModuleConfig Name=\"" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "\" AITargetType=\"" + GetModuleType() + "Module\" SimClass=\"" + GetModuleType() + "Module\" BuildBaseIndustryCost=\""
+                //    + GetInitialCost() + "\" BuildIndustryCostIncrement=\"" + GetCostIncrement() + "\" BuildDuration=\"" + GetBuildDuration() + (GetAttackType().Length != 0 ? "\" AttackType=\"" + GetAttackType() + "\"" : "\"")
+                //    + (GetAttractsMerchant() ? " AttractMerchant=\"1\"" : "") + (GetPowersRoom() ? " PowersRoom=\"true\"" : "") + (GetNeedRoomPower() ? "" : " NeedRoomPower=\"0\"") + (GetRotate() ? " Rotate=\"1\"" : "")
+                //    + (GetUnremovable() ? " Unremovable=\"1\"" : "") + "/>");
+                //Util.ApplyFileChange(@"Public\Configuration\ModuleConfigs.xml", "Minor0017", 1, linesLst);
+                //// BluePrintConfig
+                //linesLst = new List<string>();
+                //for (int i = 1; i <= GetLevels(); i++) {
+                //    linesLst.Add("  <BluePrintConfig Name=\"" + GetModuleType() + "Module_" + GetModuleType() + GetID() + "_LVL" + i + "\" ModuleCategory=\"" + GetModuleCategory() + "\" ModuleName=\""
+                //        + GetModuleType() + "Module_" + GetModuleType() + GetID() + "\" ModuleLevel=\"" + i + "\" ResearchScienceCost=\"" + GetResearchCosts()[i - 1] + "\"/>");
+                //}
+                //Util.ApplyFileChange(@"Public\Configuration\BluePrintConfigs.xml", "Minor0017", 1, linesLst);
             } else
             {
                 Log("Attempting to remove Localization changes!");
                 Util.RemoveLocalizationChangeInclusive(GetModuleType() + "Module_", GetModuleType() + "Module_" + GetModuleType() + GetID() + "_Description");
-                Util.RemoveFileChangeInclusive(@"Public\Configuration\ModuleConfigs.xml", GetID(), GetID());
-                Util.RemoveFileChangeInclusive(@"Public\Configuration\BluePrintConfigs.xml", GetID(), GetID());
+                //Util.RemoveFileChangeInclusive(@"Public\Configuration\ModuleConfigs.xml", GetID(), GetID());
+                //Util.RemoveFileChangeInclusive(@"Public\Configuration\BluePrintConfigs.xml", GetID(), GetID());
                 // Remove Localization Changes
             }
             Log("Initialized!");
@@ -103,14 +161,105 @@ namespace DustDevilFramework
             base.Load();
             if (settings.Enabled)
             {
-
+                On.Module.Init += Module_Init;
+                On.Module.InitAnims += Module_InitAnims;
+                On.MainGameScreen.Awake += MainGameScreen_Awake;
             }
         }
+
+        private void AddBPConfigsToDatabase()
+        {
+            Log("Adding " + GetLevels() + " BluePrintConfigs to Database!");
+            for (int level = 1; level <= GetLevels(); level++) {
+                BluePrintConfig bpConfig = new BluePrintConfig();
+                DynData<BluePrintConfig> d = new DynData<BluePrintConfig>(bpConfig);
+                bpConfig.XmlSerializableName = GetModuleType() + "Module_" + GetModuleType() + GetID();
+                d.Set("ModuleName", GetModuleType() + "Module_" + GetModuleType() + GetID());
+                d.Set("ModuleCategory", GetModuleCategory());
+                d.Set("ModuleLevel", level);
+                d.Set("ResearchScienceCost", GetResearchCosts()[level - 1]);
+                Databases.GetDatabase<BluePrintConfig>(false).Add(bpConfig);
+            }
+        }
+
+        private void AddModuleConfigToDatabase()
+        {
+            Log("Adding ModuleConfig to Database!");
+            ModuleConfig config = new ModuleConfig();
+            DynData<ModuleConfig> d = new DynData<ModuleConfig>(config);
+            config.XmlSerializableName = GetModuleType() + "Module_" + GetModuleType() + GetID();
+            config.XmlSerializableAITargetType = GetModuleType() + "Module";
+            d.Set("AttackType", GetAttackType());
+            d.Set("SimClass", GetModuleType() + "Module");
+            d.Set("AttractMerchant", GetAttractsMerchant());
+            d.Set("BuildBaseIndustryCost", GetInitialCost());
+            d.Set("BuildDuration", GetBuildDuration());
+            d.Set("BuildIndustryCostIncrement", GetCostIncrement());
+            d.Set("NeedRoomPower", GetNeedRoomPower());
+            d.Set("PowersRoom", GetPowersRoom());
+            d.Set("Rotate", GetRotate());
+            d.Set("Unremovable", GetUnremovable());
+            Databases.GetDatabase<ModuleConfig>(false).Add(config);
+        }
+
+        private void MainGameScreen_Awake(On.MainGameScreen.orig_Awake orig, MainGameScreen self)
+        {
+            // Placeholder to simply add all the info to the Database.
+            if (!Databases.GetDatabase<BluePrintConfig>(false).ContainsKey(GetModuleType() + "Module_" + GetModuleType() + GetID() + "_LVL1"))
+            {
+                AddBPConfigsToDatabase();
+            }
+            if (!Databases.GetDatabase<ModuleConfig>(false).ContainsKey(GetModuleType() + "Module_" + GetModuleType() + GetID()))
+            {
+                AddModuleConfigToDatabase();
+            }
+            orig(self);
+        }
+
+        private void Module_InitAnims(On.Module.orig_InitAnims orig, Module self)
+        {
+            if (self.GetConfigName() == GetModuleType() + "Module_" + GetModuleType() + GetID())
+            {
+                // This is our module!
+                DynData<Module> d = new DynData<Module>(self);
+                SpriteAnimationRuntime2 baseAnim = d.Get<SpriteAnimationRuntime2>("baseSpriteAnim");
+                SpriteAnimationRuntime2 faceSprite = d.Get<SpriteAnimationRuntime2>("faceSpriteAnim");
+                SpriteAnimationRuntime2 iconSpriteAnim = d.Get<SpriteAnimationRuntime2>("iconSpriteAnim");
+                baseAnim.OverrideClipsFromPath("SpriteAnimations/Modules/Default_" + GetModuleCategory() + "/LVL1", false);
+                baseAnim.OverrideClipsFromPath("SpriteAnimations/Modules/" + GetAssumedModuleName() + "/LVL1", false);
+                faceSprite.OverrideClipsFromPath("SpriteAnimations/Modules/Default_" + GetModuleCategory() + "/LVL1", false);
+                faceSprite.OverrideClipsFromPath("SpriteAnimations/Modules/" + GetAssumedModuleName() + "/LVL1", false);
+                if (iconSpriteAnim != null)
+                {
+                    iconSpriteAnim.Stop(true);
+                }
+                return;
+            }
+            orig(self);
+        }
+
         public new void UnLoad()
         {
             base.UnLoad();
-
+            On.Module.Init -= Module_Init;
+            On.Module.InitAnims -= Module_InitAnims;
         }
+
+        private void Module_Init(On.Module.orig_Init orig, Module self, ulong ownerPlayerID, BluePrintConfig bpConfig, Room parentRoom, ModuleSlot slot, float buildDuration, bool playAppearanceAnimations, bool instantBuild, bool restoration, bool checkOwnershipForBuildComplete)
+        {
+            orig(self, ownerPlayerID, bpConfig, parentRoom, slot, buildDuration, playAppearanceAnimations, instantBuild, restoration, checkOwnershipForBuildComplete);
+            // Add the descriptor to the db, if it does anything, and then create it as normal.
+            if (GetSimulationDescriptor() == null)
+            {
+                return;
+            }
+            if (SimMonoBehaviour.GetDBDescriptorByName(GetModuleType() + "Module_" + GetModuleType() + GetID()) == null)
+            {
+                SimMonoBehaviour.GetSimDescDatabase().Add(GetSimulationDescriptor().GetDescriptor(GetModuleType() + "Module_" + GetModuleType() + GetID()));
+            }
+            self.AddSimDescriptor(SimMonoBehaviour.GetDBDescriptorByName(GetModuleType() + "Module_" + GetModuleType() + GetID()));
+        }
+
         public enum ModuleType
         {
             Minor, Major
