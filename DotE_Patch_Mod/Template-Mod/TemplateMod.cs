bc0509c4ff709315fad78eeb08b4dbc7d55f8790
Unknown <adamznow@gmail.com>
2019-04-04 02:35:07 -0700

    Added a TemplateMod (and a TemplateMod Project Template on releases)
    TODO: Need to really start working on making a custom mod loaded, Partiality is definitely not ideal, nor supported.
    LOOK INTO MAKING SOMETHING LOOK NICE AND PRETTY (LIKE MODSABER) WITH ELECTRON

---
+++ DotE_Patch_Mod/Template-Mod/TemplateMod.cs
@@ -0,0 +1,51 @@
+﻿using DustDevilFramework;
+using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Mod
+{
+    class TemplateMod : PartialityMod
+    {
+        internal ScadMod mod = new ScadMod("TemplateName", typeof(TemplateSettings), typeof(TemplateMod));
+
+        /// <summary>
+        /// This is the private reference variable to your settings that has the correct type, as oppossed to mod.settings.
+        /// This gets set to after mod.settings is read from.
+        /// </summary>
+        private TemplateSettings settings;
+
+        public override void Init()
+        {
+            mod.PartialityModReference = this;
+            mod.Initialize();
+
+            // Versioning
+            mod.MajorVersion = 0;
+            mod.MinorVersion = 0;
+            mod.Revision = 1;
+
+            mod.settings.ReadSettings();
+
+            settings = (mod.settings as TemplateSettings);
+
+            mod.Log("Initialized!");
+        }
+        public override void OnLoad()
+        {
+            mod.Load();
+            if (mod.settings.Enabled)
+            {
+                // Add hooks here!
+            }
+        }
+        public void UnLoad()
+        {
+            mod.UnLoad();
+            // Remove hooks here!
+        }
+    }
+}
---
+++ DotE_Patch_Mod/Template-Mod/TemplateSettings.cs
@@ -0,0 +1,19 @@
+﻿using DustDevilFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Mod
+{
+    class TemplateSettings : ModSettings
+    {
+        // Add fields here! Fields that are supported include ints, floats, bools, doubles, strings
+        // Other fields can be stored here, but will not be saved to a config file
+        // You can preface each field with a flag from DustDevilFramework.ModSettings
+        public TemplateSettings(string name) : base(name)
+        {
+        }
+    }
+}
