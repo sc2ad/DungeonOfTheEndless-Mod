e005f0efc0bf8229a42156021e59a7e81132829d
Adam ? <adamznow@gmail.com>
2019-04-07 16:23:44 -0700

    Started work on this stuff, but currently does not work.
    Will fix this at some point in the future, but definitely getting close to a TAS-able run (as well as seeded runs!)

---
+++ DotE_Patch_Mod/GotoFloor-Mod/GotoFloorMod.cs
@@ -0,0 +1,64 @@
+﻿using DustDevilFramework;
+using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotoFloor_Mod
+{
+    class GotoFloorMod : PartialityMod
+    {
+        internal ScadMod mod = new ScadMod("GotoFloor", typeof(GotoFloorSettings), typeof(GotoFloorMod));
+
+        /// <summary>
+        /// This is the private reference variable to your settings that has the correct type, as oppossed to mod.settings.
+        /// This gets set to after mod.settings is read from.
+        /// </summary>
+        private GotoFloorSettings settings;
+
+        public override void Init()
+        {
+            mod.PartialityModReference = this;
+            mod.Initialize();
+
+            // Versioning
+            mod.MajorVersion = 0;
+            mod.MinorVersion = 0;
+            mod.Revision = 1;
+
+            mod.settings.ReadSettings();
+
+            settings = (mod.settings as GotoFloorSettings);
+
+            mod.Log("Initialized!");
+        }
+        public override void OnLoad()
+        {
+            mod.Load();
+            if (mod.settings.Enabled)
+            {
+                On.Dungeon.PrepareForNewGame += Dungeon_PrepareForNewGame;
+                // Add hooks here!
+            }
+        }
+
+        private void Dungeon_PrepareForNewGame(On.Dungeon.orig_PrepareForNewGame orig, bool multiplayer)
+        {
+            orig(multiplayer);
+            // Get the nextDungeonGenerationParams to modify!
+            var field = typeof(Dungeon).GetField("nextDungeonGenerationParams", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            DungeonGenerationParams p = (DungeonGenerationParams)field.GetValue(null);
+            p.Level = settings.LevelTarget;
+            mod.Log("Set the nextDungeonGenerationParams to level: " + settings.LevelTarget);
+        }
+
+        public void UnLoad()
+        {
+            mod.UnLoad();
+            // Remove hooks here!
+            On.Dungeon.PrepareForNewGame -= Dungeon_PrepareForNewGame;
+        }
+    }
+}
---
+++ DotE_Patch_Mod/GotoFloor-Mod/GotoFloorSettings.cs
@@ -0,0 +1,22 @@
+﻿using DustDevilFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotoFloor_Mod
+{
+    class GotoFloorSettings : ModSettings
+    {
+        // Add fields here! Fields that are supported include ints, floats, bools, doubles, strings
+        // Other fields can be stored here, but will not be saved to a config file
+        // You can preface each field with a flag from DustDevilFramework.ModSettings
+        [SettingsRange(1, 12)]
+        public int LevelTarget = 1;
+        
+        public GotoFloorSettings(string name) : base(name)
+        {
+        }
+    }
+}
