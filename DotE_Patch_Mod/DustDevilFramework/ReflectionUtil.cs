0f291c6ac13c729aa773d6becf2fb45f5cc29c10
Adam ? <adamznow@gmail.com>
2019-06-17 21:37:59 -0700

    Updated MonoMod + BepInEx, added ReflectionUtil extensions

---
+++ DotE_Patch_Mod/DustDevilFramework/ReflectionUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DustDevilFramework
+{
+    public static class ReflectionUtil
+    {
+        public static PropertyInfo GetPrivateProperty(this Type type, string name)
+        {
+            return type.GetProperty(name, BindingFlags.NonPublic);
+        }
+
+    }
+}
